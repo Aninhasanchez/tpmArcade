ea605b3bf40fee06a86585e7e4d490460fe67d34
Aninhasanchez <analuisasanchez05@gmail.com>
2025-08-13 14:59:18 -0300

    13-08

---
+++ tpm-arcade/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-200">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Tailwind funcionando!
+      </h1>
+    </div>
+  );
+}
